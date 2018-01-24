0fc9acc836a332fbfab72490928cd7630861fb59
Ruhi Kumbhani <ruhi.kumbhani@gmail.com>
2018-01-24 00:02:07 -0800

    key config

---
+++ keys.js
@@ -0,0 +1,23 @@
+// API Keys - Ruhi Kumbhani
+
+//console.log('this is loaded');
+
+var keys = {
+	twitterKeys: {
+		consumer_key: 'ACMef5ZPrNSPDaBT2jwFeJkJ0',
+  		consumer_secret: 'dfRZVWN1E8li79FGxky9HrCFfoz1pVqMRZsvai3TsfyOK7VLnV',
+  		access_token_key: '783780650-IapjSeG807IUOZnYEGpLQ1z47FcoFMq08a2NrXfN',
+  		access_token_secret: '	N878DNoh5JVFJeh8lGe0n5GQ1s41Q7XlepD9hAOfc9txG'
+	},
+	spotifyKeys: {
+		client_id: '12a6b3b4107e4ea1bff78d89c21bf7d7',
+		client_secret: 'a86f2cf996a94d648f8b82a053c698bc'
+	},
+    omdbKeys: {
+        consumer_key: f8b248
+    }
+}
+
+
+
+module.exports = keys;
